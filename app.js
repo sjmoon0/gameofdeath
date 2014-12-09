fae914cf5d3da1b313c302f3b913d97c571893a2
Steven Moon <steven.jay.moon@gmail.com>
2014-12-09 04:57:41 -0500

    Added gameplay. Server crashes after first player takes turn.

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var numClients=0;
 var gameStarted=false;
 var numCharChosen=0;
 var allClients=[];
+var currentPlayer=0;
+
+var players;
+var boardUpdate;
 
 app.listen(8001);
 
@@ -43,7 +47,7 @@ function handler (req, res) {
           return;
         }
         else if (extension === 'jpeg') res.writeHeader(200, {"Content-Type": 'image/jpeg'});
-        else { res.writeHead(200);console.log("NO CORRECT EXTENSION");};
+        else { res.writeHead(200);};
     }
     
     //console.log(extension);
@@ -52,6 +56,7 @@ function handler (req, res) {
   });
 }
 console.log("~Server Running~");
+
 
 io.on('connection', function (socket) {
   console.log("A Client connected");
@@ -92,12 +97,18 @@ io.on('connection', function (socket) {
   socket.on('player_chosen', function(cp){
     //This is where you should assign playerID and send back to client
     //Create new player object with playerID, and chosen character, and remaining default values
-
-    console.log(cp+" has been selected");
+    for(i=0;i<4;++i){
+      if(players[i].name==cp){
+        players[i].socket=socket;
+        console.log(cp+" player created: "+players[i]);
+      }
+    }
+    socket.emit('set_user', cp);
+    //console.log(cp+" has been selected");
     numCharChosen++;
     io.emit('disable_player_choice',{'stuff':[{'chara':cp,'numChar':numCharChosen}]});
     if(numCharChosen==4){
-      io.emit('start_gameplay', {'stuff':'more stuff'});
+      io.emit('start_gameplay', boardUpdate);
     }
   });
 
@@ -108,9 +119,65 @@ io.on('connection', function (socket) {
     io.emit('client_disconnect',"We've lost another comrade!");
   });
 
-  /*
-  var movePlayer(playerID, ){
-    
-  }
-  */
+  socket.on('ready_to_play',function(characterThatIsReadyToPlay){
+    io.emit('take_turn',boardUpdate);
+  });
+
+  socket.on('roll_dice', function(characterThatRolledDice){
+    var temp=generateRollResult(characterThatRolledDice)
+    socket.emit('roll_result',temp);
+  });
+
+  socket.on('end_turn',function(characterThatEndedTurn){
+    io.emit('take_turn',nextUpdate(characterThatEndedTurn));
+  });
 });
+
+var initializePlayers=function(){
+  players=[
+    {"name":"Ape","location":0,"health":100,"money":5000,"disease":null, "order":0,"socketid":null},
+    {"name":"Bob","location":0,"health":100,"money":5000,"disease":null, "order":1,"socketid":null},
+    {"name":"Cow","location":0,"health":100,"money":5000,"disease":null, "order":2,"socketid":null},
+    {"name":"Dan","location":0,"health":100,"money":5000,"disease":null, "order":3,"socketid":null}];
+}
+
+var initializeUpdate=function(){
+  boardUpdate={"p1":0,"p2":0,"p3":0,"p4":0,"currPlayer":"Ape","prevPlayerBrief":null};
+}
+
+var generateRollResult=function(name){
+  console.log("a");
+  var r=Math.floor((Math.random() * 12) + 1);
+  console.log("b");
+  var st=1;
+  console.log("c");
+  var nl;
+  console.log("d");
+  for(i=0;i<4;++i){
+    if(players[i].name=name){
+      players[i].location=(players[i].location+r)%40;
+      nl=players[i];
+    }
+  }
+  console.log("e");
+  var cc="YOU GOT SHOT. BRO...YOU OKAY?"
+  console.log("f");
+  return {'roll':r,'spaceType':st,'newLocation':nl,'cardContent':cc};
+} 
+
+var nextUpdate=function(endingPlayer){
+  boardUpdate.p1=players[0].location;
+  boardUpdate.p2=players[1].location;
+  boardUpdate.p3=players[2].location;
+  boardUpdate.p4=players[3].location;
+  for(i=0;i<4;++i){
+      if(players[i].name=endingPlayer){
+        boardUpdate.currPlayer=players[(i+1)%4].name;
+      }
+    }
+  return boardUpdate;
+}
+
+initializePlayers();
+initializeUpdate();
+console.log("::Players initialized::");
---
+++ cards.js
@@ -0,0 +1,7 @@
+var cards='"ca":['+
+		//Regular Cards are type 1
+		'{"type":1, "content":"<insert text here>","brief":"<insert brief text here>", "used":false, "health":<healthChange integer>, "money":<moneyChange integer>, "disease":"<name of disease>", "diseaseRate":<disease integer rate> },'+
+		//Life Cut Cards are type 2
+		'{"type":2, "content":"<insert text here>","brief":"<insert brief text here>", "used":false, "health":<healthChange integer>, "money":<moneyChange integer>, "disease":"<name of disease>", "diseaseRate":<disease integer rate> },'+
+		//Good fortune cards are type 3
+		'{"type":3, "content":"<insert text here>","brief":"<insert brief text here>", "used":false, "health":<healthChange integer>, "money":<moneyChange integer>, "disease":"<name of disease>", "diseaseRate":<disease integer rate> }]';
